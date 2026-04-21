6544ec6969b60cdeebf3fb40edbe8daa9beda4fa
Danilo Silva <contact@danilocgsilva.me>
2026-04-21 17:19:22 -0300

    Improvements in server checking

--- src/extension.ts
+++ src/extension.ts
@@ -1,9 +1,16 @@
 import * as vscode from 'vscode';
-import fetch from 'node-fetch';
 
-async function isOllamaRunning() {
+function getOllamaHost(): string {
+	return 'host.docker.internal';
+}
+
+function getModel(): string {
+	return "qwen2.5-coder:latest";
+}
+
+async function isOllamaRunning(ollamaHost: string) {
 	try {
-		const res = await fetch('http://localhost:11434/api/tags');
+		const res = await fetch(`http://${ollamaHost}:11434/api/tags`);
 		return res.ok;
 	} catch {
 		return false;
@@ -13,10 +20,17 @@ async function isOllamaRunning() {
 function writeIntoCursor(): vscode.Disposable {
 	const disposable = vscode.commands.registerCommand('ai-connector.insertText', async () => {
 		const editor = vscode.window.activeTextEditor;
-
 		if (!editor) {
 			vscode.window.showErrorMessage('No active editor');
 			return;
+		}
+
+		const ollamaHost = getOllamaHost();
+		if (!(await isOllamaRunning(ollamaHost))) {
+			vscode.window.showErrorMessage('Ollama is not running. Please start Ollama and try again.');
+			return;
+		} else {
+			vscode.window.showInformationMessage('Ollama is running. Wen can begin...');
 		}
 
 		const position = editor.selection.active;
@@ -30,6 +44,7 @@ function writeIntoCursor(): vscode.Disposable {
 }
 
 export function activate(context: vscode.ExtensionContext) {
+
 	const writeIntoCursorDisposable = writeIntoCursor();
 
 	context.subscriptions.push(writeIntoCursorDisposable);
